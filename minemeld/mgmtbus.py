4a9037d7312963e1117474e5780caa2c389a3b4d
jtschichold <lmori@paloaltonetworks.com>
2015-09-16 12:09:41 +0000

    Fixed bug in status loop for global length

--- minemeld/mgmtbus.py
+++ minemeld/mgmtbus.py
@@ -149,7 +149,7 @@ class MgmtbusMaster(object):
                 cc.putval(source+'.'+m, v, interval=interval)
 
             if length is not None:
-                gstats[ntype+'.'+'length'] += v
+                gstats[ntype+'.length'] += length
                 cc.putval(
                     source+'.length',
                     length,
