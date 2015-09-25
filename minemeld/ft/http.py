6452d10486245cf3b215222b7f705612b333d1d2
jtschichold <lmori@paloaltonetworks.com>
2015-09-25 12:51:56 +0000

    Added regex handling

--- minemeld/ft/http.py
+++ minemeld/ft/http.py
@@ -4,6 +4,7 @@ import copy
 import gevent
 import gevent.event
 import random
+import re
 
 from . import base
 from . import table
@@ -31,14 +32,44 @@ class HttpFT(base.BaseFT):
 
         self.source_name = self.config.get('source_name', self.name)
         self.url = self.config.get('url', None)
-        self.cchar = self.config.get('cchar', None)
-        self.split_char = self.config.get('split_char', None)
-        self.split_pos = self.config.get('split_pos', 0)
         self.attributes = self.config.get('attributes', {})
         self.interval = self.config.get('interval', 900)
         self.polling_timeout = self.config.get('polling_timeout', 20)
         self.num_retries = self.config.get('num_retries', 2)
         self.verify_cert = self.config.get('verify_cert', True)
+
+        self.ignore_regex = self.config.get('ignore_regex', None)
+        if self.ignore_regex is not None:
+            self.ignore_regex = re.compile(self.ignore_regex)
+
+        self.indicator = self.config.get('indicator', None)
+
+        if self.indicator is not None:
+            if 'regex' in self.indicator:
+                self.indicator['regex'] = re.compile(self.indicator['regex'])
+            else:
+                raise ValueError('%s - indicator stanza should have a regex',
+                                 self.name)
+            if 'transform' not in self.indicator:
+                if self.indicator['regex'].groups > 0:
+                    LOG.warning('%s - no transform string for indicator'
+                                ' but pattern contains groups',
+                                self.name)
+                self.indicator['transform'] = '\g<0>'
+
+        self.fields = self.config.get('fields', {})
+        for f, fattrs in self.fields.iteritems():
+            if 'regex' in fattrs:
+                fattrs['regex'] = re.compile(fattrs['regex'])
+            else:
+                raise ValueError('%s - %s field does not have a regex',
+                                 self.name, f)
+            if not 'transform' in fattrs:
+                if fattrs['regex'].groups > 0:
+                    LOG.warning('%s - no transform string for field %s'
+                                ' but pattern contains groups',
+                                self.name, f)
+                fattrs['transform'] = '\g<0>'
 
     def rebuild(self):
         self.rebuild_flag = True
@@ -54,8 +85,34 @@ class HttpFT(base.BaseFT):
         self.idle_waitobject.set(value)
 
     def _process_line(self, line):
-        attributes = copy.deepcopy(self.attributes)
-        return line.split()[0], attributes
+        if self.indicator is None:
+            indicator = line.split()[0]
+
+        else:
+            indicator = self.indicator['regex'].search(line)
+            if indicator is None:
+                return None, None
+
+            indicator = indicator.expand(self.indicator['transform'])
+
+        attributes = copy.copy(self.attributes)
+
+        for f, fattrs in self.fields.iteritems():
+            m = fattrs['regex'].search(line)
+
+            if m is None:
+                continue
+
+            attributes[f] = m.expand(fattrs['transform'])
+
+            try:
+                i = int(attributes[f])
+            except:
+                pass
+            else:
+                attributes[f] = i
+
+        return indicator, attributes
 
     def _polling_loop(self):
         LOG.info("Polling %s", self.name)
@@ -79,15 +136,9 @@ class HttpFT(base.BaseFT):
             if not line:
                 continue
 
-            if self.cchar is not None and \
-               line.startswith(self.cchar):
-                continue
-
-            if self.split_char is not None:
-                toks = line.split(self.split_char)
-                if len(toks) < self.split_pos+1:
+            if self.ignore_regex is not None:
+                if self.ignore_regex.match(line) is not None:
                     continue
-                line = toks[self.split_pos].strip()
 
             indicator, attributes = self._process_line(line)
             if indicator is None:
