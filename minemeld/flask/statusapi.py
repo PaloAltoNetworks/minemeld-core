5217865c3c5ef63f80c2afe5a953ff598a04c760
Luigi Mori <lmori@paloaltonetworks.com>
2017-01-18 03:06:55 +0000

    Adds backup API
    Signed-off-by: Luigi Mori <lmori@paloaltonetworks.com>

--- minemeld/flask/statusapi.py
+++ minemeld/flask/statusapi.py
@@ -12,13 +12,18 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import psutil
 import os
-import yaml
+import os.path
 import uuid
+import functools
 import time
+from zipfile import ZipFile
+from tempfile import NamedTemporaryFile
 
-from flask import Response, stream_with_context, jsonify, request
+import gevent
+import psutil
+import yaml
+from flask import Response, stream_with_context, jsonify, request, send_file
 
 from . import config
 from .mmrpc import MMMaster
@@ -27,6 +32,7 @@ from .mmrpc import MMRpcClient
 from .redisclient import SR
 from .aaa import MMBlueprint
 from .logger import LOG
+from .jobs import JOBS_MANAGER
 
 
 __all__ = ['BLUEPRINT']
@@ -222,3 +228,81 @@ def signal_node(nodename, signalname):
     )
 
     return jsonify(result='ok'), 200
+
+
+def _clean_local_backup(local_backup_file, g):
+    def _safe_remove(path):
+        LOG.info('Removing backup {}'.format(local_backup_file))
+        try:
+            os.remove(path)
+        except:
+            pass
+
+    if g.value != 0:
+        _safe_remove(local_backup_file)
+        return
+
+    LOG.info('Removing backup {} in 300s'.format(local_backup_file))
+    gevent.spawn_later(300, _safe_remove, local_backup_file)
+
+
+# XXX this should be moved to a different endpoint
+@BLUEPRINT.route('/backup', methods=['POST'], read_write=True)
+def generate_local_backup():
+    params = request.get_json(silent=True)
+    if params is None:
+        return jsonify(error={'message': 'missing request body'}), 400
+
+    password = params.get('p', None)
+    if password is None:
+        return jsonify(error={'message': 'missing p paramater in request body'}), 400
+
+    sevenz_path = config.get('MINEMELD_7Z_PATH', None)
+    if sevenz_path is None:
+        return jsonify(error={'message': 'MINEMELD_7Z_PATH not set'}), 500
+
+    # create temp zip file
+    tf = NamedTemporaryFile(prefix='mm-local-backup', suffix='.zip', delete=False)
+    tf.close()
+    # initialize the zip structure inside the file
+    ZipFile(tf.name, 'w').close()
+
+    # build args
+    args = [sevenz_path, 'a', '-p{}'.format(password), '-y', tf.name]
+
+    library_path = config.get('MINEMELD_LOCAL_LIBRARY_PATH', None)
+    if library_path is not None:
+        args.append(library_path)
+    proto_path = config.get('MINEMELD_LOCAL_PROTOTYPE_PATH', None)
+    if proto_path is not None:
+        args.append(proto_path)
+    config_path = os.path.dirname(os.environ.get('MM_CONFIG'))
+    args.append(config_path)
+
+    jobs = JOBS_MANAGER.get_jobs(job_group='status-backup')
+    for jobid, jobdata in jobs.iteritems():
+        if jobdata == 'RUNNING':
+            return jsonify(error={'message': 'a backup job is already running'}), 400
+
+    jobid = JOBS_MANAGER.exec_job(
+        job_group='status-backup',
+        description='local backup',
+        args=args,
+        data={
+            'result-file': tf.name
+        },
+        callback=functools.partial(_clean_local_backup, tf.name)
+    )
+
+    return jsonify(result=jobid)
+
+
+# XXX this should be moved to a different endpoint
+@BLUEPRINT.route('/backup/<jobid>', methods=['GET'], read_write=True)
+def get_local_backup(jobid):
+    jobs = JOBS_MANAGER.get_jobs(job_group='status-backup')
+
+    if jobid not in jobs:
+        return jsonify(error={'message': 'unknown job'}), 404
+
+    return send_file(jobs[jobid]['result-file'])
