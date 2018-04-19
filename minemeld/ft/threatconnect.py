91eed7759df9950b285adad151e67118fe12d363
Xavier Homs <xhoms@paloaltonetworks.com>
2018-04-19 16:55:53 +0200

    Honors prototype API KEY and SECRET if not provided in the side config.

--- minemeld/ft/threatconnect.py
+++ minemeld/ft/threatconnect.py
@@ -245,11 +245,12 @@ class TCMiner(BasePollerFT):
             LOG.error('%s - Error loading side config: %s', self.name, str(e))
             return
 
-        self.tc.api_key = sconfig.get('apikey', None)
-        self.tc.api_secret = sconfig.get('apisecret', None)
-        data_owner = sconfig.get('owner', None)
-        if data_owner is not None:
-            self.tc.owner = quote(data_owner)
+        self.tc = None
+        data_owner = sconfig.get('owner', self.owner)
+        side_api_key = sconfig.get('apikey', self.api_key)
+        side_api_secret = sconfig.get('apisecret', self.api_secret)
+        if not (None in [side_api_key, side_api_secret]):
+            self.tc = ThreatConnect(side_api_secret, side_api_key, self.api_url, self.api_base_uri, data_owner)
 
     def _saved_state_restore(self, saved_state):
         super(TCMiner, self)._saved_state_restore(saved_state)
