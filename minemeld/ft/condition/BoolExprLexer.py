ac7a9523651d52c9076c23139217177918c7b379
Luigi Mori <l@isidora.org>
2019-09-12 11:07:04 +0200

    Updated antlr4 runtime to 4.7
    Signed-off-by: Luigi Mori <l@isidora.org>

--- minemeld/ft/condition/BoolExprLexer.py
+++ minemeld/ft/condition/BoolExprLexer.py
@@ -1,8 +1,9 @@
-# Generated from BoolExpr.g4 by ANTLR 4.5.1
+# Generated from BoolExpr.g4 by ANTLR 4.7.1
 # encoding: utf-8
 from __future__ import print_function
 from antlr4 import *
 from io import StringIO
+import sys
 
 
 # flake8: noqa
@@ -10,7 +11,7 @@ from io import StringIO
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write(u"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\2")
+        buf.write(u"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2")
         buf.write(u"\22z\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
         buf.write(u"\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t")
         buf.write(u"\r\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22")
@@ -25,33 +26,33 @@ def serializedATN():
         buf.write(u"\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f\27\r\31")
         buf.write(u"\16\33\17\35\20\37\21!\2#\22\3\2\t\6\2&&C\\aac|\7\2&")
         buf.write(u"&\62;C\\aac|\4\2$$^^\4\2))^^\3\2\63;\3\2\62;\5\2\13\f")
-        buf.write(u"\17\17\"\"\u0080\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2")
-        buf.write(u"\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2")
-        buf.write(u"\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2\2")
-        buf.write(u"\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2")
-        buf.write(u"#\3\2\2\2\3%\3\2\2\2\5\'\3\2\2\2\7)\3\2\2\2\t+\3\2\2")
-        buf.write(u"\2\13-\3\2\2\2\r\60\3\2\2\2\17\63\3\2\2\2\21\66\3\2\2")
-        buf.write(u"\2\238\3\2\2\2\25;\3\2\2\2\27@\3\2\2\2\31F\3\2\2\2\33")
-        buf.write(u"K\3\2\2\2\35b\3\2\2\2\37e\3\2\2\2!q\3\2\2\2#t\3\2\2\2")
-        buf.write(u"%&\7*\2\2&\4\3\2\2\2\'(\7+\2\2(\6\3\2\2\2)*\7.\2\2*\b")
-        buf.write(u"\3\2\2\2+,\7>\2\2,\n\3\2\2\2-.\7>\2\2./\7?\2\2/\f\3\2")
-        buf.write(u"\2\2\60\61\7?\2\2\61\62\7?\2\2\62\16\3\2\2\2\63\64\7")
-        buf.write(u"@\2\2\64\65\7?\2\2\65\20\3\2\2\2\66\67\7@\2\2\67\22\3")
-        buf.write(u"\2\2\289\7#\2\29:\7?\2\2:\24\3\2\2\2;<\7v\2\2<=\7t\2")
-        buf.write(u"\2=>\7w\2\2>?\7g\2\2?\26\3\2\2\2@A\7h\2\2AB\7c\2\2BC")
-        buf.write(u"\7n\2\2CD\7u\2\2DE\7g\2\2E\30\3\2\2\2FG\7p\2\2GH\7w\2")
-        buf.write(u"\2HI\7n\2\2IJ\7n\2\2J\32\3\2\2\2KO\t\2\2\2LN\t\3\2\2")
-        buf.write(u"ML\3\2\2\2NQ\3\2\2\2OM\3\2\2\2OP\3\2\2\2P\34\3\2\2\2")
-        buf.write(u"QO\3\2\2\2RV\7$\2\2SU\n\4\2\2TS\3\2\2\2UX\3\2\2\2VT\3")
-        buf.write(u"\2\2\2VW\3\2\2\2WY\3\2\2\2XV\3\2\2\2Yc\7$\2\2Z^\7)\2")
-        buf.write(u"\2[]\n\5\2\2\\[\3\2\2\2]`\3\2\2\2^\\\3\2\2\2^_\3\2\2")
-        buf.write(u"\2_a\3\2\2\2`^\3\2\2\2ac\7)\2\2bR\3\2\2\2bZ\3\2\2\2c")
-        buf.write(u"\36\3\2\2\2df\7/\2\2ed\3\2\2\2ef\3\2\2\2fg\3\2\2\2gh")
-        buf.write(u"\5!\21\2h \3\2\2\2ir\7\62\2\2jn\t\6\2\2km\t\7\2\2lk\3")
-        buf.write(u"\2\2\2mp\3\2\2\2nl\3\2\2\2no\3\2\2\2or\3\2\2\2pn\3\2")
-        buf.write(u"\2\2qi\3\2\2\2qj\3\2\2\2r\"\3\2\2\2su\t\b\2\2ts\3\2\2")
-        buf.write(u"\2uv\3\2\2\2vt\3\2\2\2vw\3\2\2\2wx\3\2\2\2xy\b\22\2\2")
-        buf.write(u"y$\3\2\2\2\13\2OV^benqv\3\b\2\2")
+        buf.write(u"\17\17\"\"\2\u0080\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2")
+        buf.write(u"\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2")
+        buf.write(u"\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2")
+        buf.write(u"\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2")
+        buf.write(u"\2#\3\2\2\2\3%\3\2\2\2\5\'\3\2\2\2\7)\3\2\2\2\t+\3\2")
+        buf.write(u"\2\2\13-\3\2\2\2\r\60\3\2\2\2\17\63\3\2\2\2\21\66\3\2")
+        buf.write(u"\2\2\238\3\2\2\2\25;\3\2\2\2\27@\3\2\2\2\31F\3\2\2\2")
+        buf.write(u"\33K\3\2\2\2\35b\3\2\2\2\37e\3\2\2\2!q\3\2\2\2#t\3\2")
+        buf.write(u"\2\2%&\7*\2\2&\4\3\2\2\2\'(\7+\2\2(\6\3\2\2\2)*\7.\2")
+        buf.write(u"\2*\b\3\2\2\2+,\7>\2\2,\n\3\2\2\2-.\7>\2\2./\7?\2\2/")
+        buf.write(u"\f\3\2\2\2\60\61\7?\2\2\61\62\7?\2\2\62\16\3\2\2\2\63")
+        buf.write(u"\64\7@\2\2\64\65\7?\2\2\65\20\3\2\2\2\66\67\7@\2\2\67")
+        buf.write(u"\22\3\2\2\289\7#\2\29:\7?\2\2:\24\3\2\2\2;<\7v\2\2<=")
+        buf.write(u"\7t\2\2=>\7w\2\2>?\7g\2\2?\26\3\2\2\2@A\7h\2\2AB\7c\2")
+        buf.write(u"\2BC\7n\2\2CD\7u\2\2DE\7g\2\2E\30\3\2\2\2FG\7p\2\2GH")
+        buf.write(u"\7w\2\2HI\7n\2\2IJ\7n\2\2J\32\3\2\2\2KO\t\2\2\2LN\t\3")
+        buf.write(u"\2\2ML\3\2\2\2NQ\3\2\2\2OM\3\2\2\2OP\3\2\2\2P\34\3\2")
+        buf.write(u"\2\2QO\3\2\2\2RV\7$\2\2SU\n\4\2\2TS\3\2\2\2UX\3\2\2\2")
+        buf.write(u"VT\3\2\2\2VW\3\2\2\2WY\3\2\2\2XV\3\2\2\2Yc\7$\2\2Z^\7")
+        buf.write(u")\2\2[]\n\5\2\2\\[\3\2\2\2]`\3\2\2\2^\\\3\2\2\2^_\3\2")
+        buf.write(u"\2\2_a\3\2\2\2`^\3\2\2\2ac\7)\2\2bR\3\2\2\2bZ\3\2\2\2")
+        buf.write(u"c\36\3\2\2\2df\7/\2\2ed\3\2\2\2ef\3\2\2\2fg\3\2\2\2g")
+        buf.write(u"h\5!\21\2h \3\2\2\2ir\7\62\2\2jn\t\6\2\2km\t\7\2\2lk")
+        buf.write(u"\3\2\2\2mp\3\2\2\2nl\3\2\2\2no\3\2\2\2or\3\2\2\2pn\3")
+        buf.write(u"\2\2\2qi\3\2\2\2qj\3\2\2\2r\"\3\2\2\2su\t\b\2\2ts\3\2")
+        buf.write(u"\2\2uv\3\2\2\2vt\3\2\2\2vw\3\2\2\2wx\3\2\2\2xy\b\22\2")
+        buf.write(u"\2y$\3\2\2\2\13\2OV^benqv\3\b\2\2")
         return buf.getvalue()
 
 
@@ -60,7 +61,6 @@ class BoolExprLexer(Lexer):
     atn = ATNDeserializer().deserialize(serializedATN())
 
     decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
-
 
     T__0 = 1
     T__1 = 2
@@ -79,6 +79,8 @@ class BoolExprLexer(Lexer):
     NUMBER = 15
     WS = 16
 
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
     modeNames = [ u"DEFAULT_MODE" ]
 
     literalNames = [ u"<INVALID>",
@@ -95,9 +97,9 @@ class BoolExprLexer(Lexer):
 
     grammarFileName = u"BoolExpr.g4"
 
-    def __init__(self, input=None):
-        super(BoolExprLexer, self).__init__(input)
-        self.checkVersion("4.5.1")
+    def __init__(self, input=None, output=sys.stdout):
+        super(BoolExprLexer, self).__init__(input, output=output)
+        self.checkVersion("4.7.1")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
